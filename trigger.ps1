5f9c08f093fd715c3e19ea862ced0ae6cedc953c
Andrea Forlin <andrea.forlin@dataservices.it>
2022-02-24 20:04:50 +0100

    fix: dev: trigger

--- trigger.ps1
+++ trigger.ps1
@@ -1,7 +1,10 @@
 
 @("https://it.wikipedia.org/wiki/9-1-1:_Lone_Star",
     "https://it.wikipedia.org/wiki/The_Good_Doctor_(serie_televisiva)",
-    "https://it.wikipedia.org/wiki/9-1-1_(serie_televisiva)"
+    "https://it.wikipedia.org/wiki/9-1-1_(serie_televisiva)",
+    "https://it.wikipedia.org/wiki/Station_19",
+    "https://it.wikipedia.org/wiki/The_Resident_(serie_televisiva)",
+    "https://it.wikipedia.org/wiki/Transplant"
 ) | ForEach-Object {
     write-host $_
     invoke-restmethod -method POST http://localhost:5000/api/parse -body @{"url" = $_ }
