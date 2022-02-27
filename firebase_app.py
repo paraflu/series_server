30db7939deb33ac5b2e0b6ee183a5c6d61997cf5
Andrea Forlin <andrea.forlin@gmail.com>
2022-02-27 21:30:13 +0100

    fix: dev: set config

--- firebase_app.py
+++ firebase_app.py
@@ -1,4 +1,6 @@
 
+import base64
+import json
 import os
 import firebase_admin
 from firebase_admin import credentials
@@ -6,7 +8,11 @@ from firebase_admin import credentials
 default_app = None
 if default_app is None:
     if 'CRED_STORE' in os.environ:
-        cred = credentials.Certificate(os.environ['CRED_STORE'])
+        if os.path.isfile(os.environ['CRED_STORE']):
+            data = json.load(os.environ["CRED_STORE"])
+        else:
+            data = json.loads(base64.b64decode(os.environ['CRED_STORE']))
+        cred = credentials.Certificate(data)
         default_app = firebase_admin.initialize_app(cred)
     else:
         raise Exception("CRED_STORE not set")
